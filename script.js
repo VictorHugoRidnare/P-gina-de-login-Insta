63fe8f42c79062b3466cdddc05701b118ecad1fc
Victor Hugo <victorhridnare@gmail.com>
2022-09-27 11:26:58 -0400

    commit inicial

---
+++ script.js
@@ -0,0 +1,8 @@
+// function hidePW(){
+//     var getClick = document.getElementById("pwInput");
+//     if(getClick.type === "password"){
+//         getClick.type = "text";
+//     } else {
+//         getClick.type = "password";
+//     }
+// }
